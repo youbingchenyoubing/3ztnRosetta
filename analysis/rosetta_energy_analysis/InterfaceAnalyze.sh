89347189a3321639ce7ffda048becf76a780c1ff
youbingchenyoubing <youbingchen@163.com>
2017-02-27 18:44:12 +0800

    update analyze file

---
+++ analysis/rosetta_energy_analysis/InterfaceAnalyze.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+InterfaceAnalyzer.linuxgccrelease @interface.flags -database ~/rosetta3.5/rosetta_source/rosetta_database -in:file:s 3ztn.pdb
---
+++ analysis/rosetta_energy_analysis/fixbb.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+fixbb.default.linuxgccrelease @min_flags -database ~/rosetta3.5/rosetta_source/rosetta_database -in:file:s 1ukf_epitobe_scaffold.pdb -packing:resfile ./1ukf_epitobe_scaffold.resfile
